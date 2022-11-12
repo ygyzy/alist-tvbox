fb43f5a045a5bd713ee7f92cd2d3a2d797af8a8a
Harold <power0721@gmail.com>
2022-11-12 18:27:16 +0800

    clean up code

--- src/main/java/cn/har01d/alist_tvbox/tvbox/MovieList.java
+++ src/main/java/cn/har01d/alist_tvbox/tvbox/MovieList.java
@@ -7,9 +7,5 @@ import java.util.List;
 
 @Data
 public class MovieList {
-    private int page = 1;
-    private int pagecount = 1;
-    private int limit;
-    private int total;
     private List<MovieDetail> list = new ArrayList<>();
 }

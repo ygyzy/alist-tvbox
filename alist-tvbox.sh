bb1e506d7a683299b0e48387ed84b1a520cb5914
ygyzy <70911240+ygyzy@users.noreply.github.com>
2022-11-16 17:18:28 +0800

    Add files via upload

---
+++ alist-tvbox.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#进行ubuntu系统与软件更新
+   sudo apt-get  update
+   sudo  apt-get upgrade
+
+
+#建立文件目录进入目录
+  mkdir -p /root/sd/pywork/ && cd /root/sd/pywork/
+
+
+
+#选择github源
+  wget https://github.com/power721/alist-tvbox/releases/download/v1.1.1/alist-tvbox-1.0.jar
+  
+ 
+#运行
+  java -jar alist-tvbox-1.0.jar
